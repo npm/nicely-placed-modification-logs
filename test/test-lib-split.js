8d19362b4ad8400f7f81eb44f074bf98f273dddb
Ryan Day <soldair@gmail.com>
2015-12-16 06:16:20 -0800

    split works!

--- test/test-lib-split.js
+++ test/test-lib-split.js
@@ -3,13 +3,15 @@ var split = require("../lib/split")
 
 test("can split",function(t){
 
-  t.plan(9)
+  t.plan(10)
 
   var s = split()
 
   var stage = 1
   var buf = []
+  var all = []
   s.on('data',function(b){
+    all.push(b)
     if(stage === 1) {
       
       buf.push(b)
@@ -17,8 +19,9 @@ test("can split",function(t){
         setImmediate(function(){
           t.equals(buf.length,3,'should have 3 items')
           t.equals(buf[0]+'','1\n','should include newlines')
-          t.equals(buf[1].start,2,'should have correct start position')
-          t.equals(buf[2].start,buf[1].start+buf[1].length,'should have correct start offset calculated from previous')
+          t.equals(buf[1].start,2,'should have correct start position ['+buf[1].start+']')
+          t.equals(buf[2].start,buf[1].start+buf[1].length,'should have correct start offset calculated from previous ')
+
           t.equals(s.fragment+'','4','should have 4 in fragment')
           stage = 2
           buf = []
@@ -29,22 +32,25 @@ test("can split",function(t){
       buf.push(b)
       if(buf.length === 4){
         t.equals(buf[buf.length-1]+'','7\n','should have last line')
-        t.equals(s.fragment.length,0,'should have empty fragment')
+        // allow fragment to be set
+        setImmediate(function(){
+          t.equals(s.fragment.length,0,'should have empty fragment')
 
-        stage = 3
-        buf = []
+          stage = 3
+          buf = []
 
-        s.write('end fragment')
+          s.write('end fragment')
 
-        s.end()
+          s.end()
+        })
       }
     }
   })
 
   s.on('fragment',function(f){
     t.equals(f+'','end fragment','should emit fragment')
-    t.equals(f.start,13,'fragment start should be correct')
-    
+    t.equals(f.start,14,'fragment start should be correct ')
+    t.equals(Buffer.concat(all)+'',"1\n2\n3\n4\n5\n6\n7\n","should have emitted all data")
   })
 
   s.write("1\n2\n3\n4")
