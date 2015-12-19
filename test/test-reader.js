fed34ef293629e90cc55bc3709e95248dda13520
Ryan Day <soldair@gmail.com>
2015-12-19 09:03:24 -0800

    some more wip for reader

---
+++ test/test-reader.js
@@ -0,0 +1,67 @@
+var reader = require('../lib/reader')
+var test = require('tape')
+
+test("validate sequence false sequence",function(t){
+  var suffix = '.log'
+  var list = {order:['a.log','b.log','c.log','d.log']}
+
+  var seq = false;
+  var result = reader.validateSequence(list,seq,suffix)
+
+  console.log(result)
+
+  t.equals(result.offset,0,"should be starting at the beginnning of a new file")
+  t.equals(result.file,'a','should be on file a')
+  t.equals(result.path,'a.log','should have correct path')
+  t.end()
+})
+
+
+test("validate sequence valid sequence",function(t){
+  var suffix = '.log'
+  var list = {order:['a.log','b.log','c.log','d.log']}
+
+  var seq = {offset:10,file:'b',path:'b.log'}
+  var result = reader.validateSequence(list,seq,suffix)
+
+  console.log(result)
+
+  t.equals(result.offset,10,"should have preserved offset")
+  t.equals(result.file,'b','should be on file specified')
+  t.equals(result.path,'b.log','should have correct path')
+  t.end()
+})
+
+test("validate sequence missing expected sequence file",function(t){
+  var suffix = '.log'
+  var list = {order:['a.log','c.log','d.log']}
+
+  var seq = {offset:10,file:'b',path:'b.log'}
+  var result = reader.validateSequence(list,seq,suffix)
+
+  console.log(result)
+
+  t.equals(result.offset,0,"should start from 0")
+  t.equals(result.file,'c','should be on file specified')
+  t.equals(result.path,'c.log','should have correct path')
+  t.end()
+})
+
+// this behavior is probably not a good way to handle this but it's documented
+test("sequence doesnt match any valid sequence but is larger than the largest sequence",function(t){
+  var suffix = '.log'
+  var list = {order:['a.log','b.log','c.log','d.log']}
+
+
+  var seq = {offset:10,file:'e',path:'e.log'}
+  var result = reader.validateSequence(list,seq,suffix)
+
+  console.log(result)
+
+  t.equals(result.offset,0,"should be starting at the beginnning of a new file")
+  t.equals(result.file,'a','should be on file a')
+  t.equals(result.path,'a.log','should have correct path')
+  t.end()
+})
+
+
